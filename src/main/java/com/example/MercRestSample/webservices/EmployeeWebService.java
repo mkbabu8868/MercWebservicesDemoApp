91c85257afc768a1002a38422c017319ded0ab0b
Mohamed Qamruddin <mqamrudd@global.sial.com>
2022-01-21 17:42:21 +0530

    commit

--- src/main/java/com/example/MercRestSample/webservices/EmployeeWebService.java
+++ src/main/java/com/example/MercRestSample/webservices/EmployeeWebService.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.measure.quantity.Mass;
 import javax.validation.Valid;
-
+import static javax.measure.unit.SI.KILOGRAM;
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -61,13 +64,34 @@ public class EmployeeWebService {
 	}
 
 	@DeleteMapping("/employees/{id}")
-	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long empId) throws ResourceNotFoundException {
+	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long empId)
+			throws ResourceNotFoundException {
 		Map<String, Boolean> response = new HashMap<>();
-		EmployeeWebModel emp = employeeRespository.findById(empId).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + empId));
-		
+		EmployeeWebModel emp = employeeRespository.findById(empId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + empId));
+
 		employeeRespository.delete(emp);
 		response.put("Deleted", Boolean.TRUE);
 		return response;
 
 	}
+
+	@GetMapping("/confDemo")
+    public Map<String, String> confDemo() {
+ 
+        Map<String, String> response = new HashMap<>();
+        RelativisticModel.select();
+        String energy = System.getenv().get("ENERGY");
+ 
+        if (energy == null) {
+            energy = "72 GeV";
+        }
+ 
+        Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+ 
+        response.put("science", "E=mc^2: " + energy + " = " + m.toString());
+ 
+        return response;
+ 
+    }
 }
